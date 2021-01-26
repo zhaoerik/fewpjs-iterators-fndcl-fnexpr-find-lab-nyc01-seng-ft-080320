24e84492dce10f2ae9494c278764b4f15c52e23d
Erik Zhao <erikzhao98@gmail.com>
2021-01-26 21:27:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 function superbowlWin(array) {
-  let win = array.find(object => {
-    object.result === "W"
-  })
+  let win = array.find(object => object.result === "W")
   return win.year ? win.year : undefined
 }
-
