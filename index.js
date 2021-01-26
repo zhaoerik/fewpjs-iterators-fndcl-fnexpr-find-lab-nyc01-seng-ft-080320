f3d11e8f041520578d7dee090ccce0903d477b82
Erik Zhao <erikzhao98@gmail.com>
2021-01-26 21:09:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const testVar = {}
+
+function testFunc() {
+  return "hi"
+}
+
+function superbowlWin(array) {
+  array.find(object => {
+    if (object.result === "W") {
+      return object.year
+    } else {
+      return undefined
+    }
+  })
+}
