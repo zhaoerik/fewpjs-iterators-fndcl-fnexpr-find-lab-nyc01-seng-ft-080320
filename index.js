7a67aa262f12916f44969d0b8a53a3d3c8e79ef2
Erik Zhao <erikzhao98@gmail.com>
2021-01-26 21:09:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,12 +4,6 @@ function testFunc() {
   return "hi"
 }
 
-const testVar = {}
-
-function testFunc() {
-  return "hi"
-}
-
 function superbowlWin(array) {
   array.find(object => {
     if (object.result === "W") {
